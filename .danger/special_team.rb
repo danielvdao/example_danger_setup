6d5942dcaf8afee39f1b4673d7b3ca2cff96ad07
Daniel Dao <daniel.dao@chime.com>
2021-04-29 00:39:01 -0500

    Fix team

--- .danger/special_team.rb
+++ .danger/special_team.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # Check that there are both app and test code changes for the main app
-changed_spot_files = CHANGED_FILES.select { |path| path =~ /^(fixtures|app)/ }
+changed_team_files = CHANGED_FILES.select { |path| path =~ /^(fixtures|app)/ }
 
-if changed_app_files.any?
+if changed_team_files.any?
     fail 'Please provide a summary in the Pull Request description' if github.pr_body.length < 5
 
     fail 'Please add title header [SPOT-XXXX] to your PR' unless github.pr_title.include? '[SPOT]'
